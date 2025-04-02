6771c3824064b5b48f0d98c951c925b8f5f3b867
unknown <seahorse2001@naver.com>
2025-04-02 12:22:54 +0900

    [0402] 이유정 blog

---
+++ blog/blog_prj/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from .models import Post
+
+
+def list(request):
+    posts = Post.objects.all().order_by('-id')
+    return render(request, 'blog/list.html', {'posts':posts})
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        concent = request.POST.get('content')
+
+        post = Post.objects.create(
+            title = title,
+            content = content
+        )
+        return redirect('blog:list')
+    return render(request, 'blog/create.html')
+
+def detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/detail.html', {'post':post})
+
+def update(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('blog:detail', id)
+    return render(request, 'blog/update.html', {'post':post})
+
+def delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    return redirect('blog:list')
