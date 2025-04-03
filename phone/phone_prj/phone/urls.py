0d8142e718042833e83261fb45d8d15ea7220d7b
unknown <seahorse2001@naver.com>
2025-04-03 18:34:18 +0900

    [0403] 이유정 phone_test

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import list, result
+from .views import list, result, create, detail, update, delete
+
+app_name = 'phone'
 
 urlpatterns = [
     path('', list, name = 'list'),
     path('result/', result, name='result'),
+    path('create/', create, name = 'create'),
+    path('detail/<int:id>/', detail, name = 'detail'),
+    path('update/<int:id>/', update, name = 'update'),
+    path('delete/<int:id>/', delete, name = 'delete'),
 ]
--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ContactInfo
 
 def list(request):
@@ -9,3 +9,41 @@ def result(request):
     keyword = request.GET.get('keyword')
     contacts = ContactInfo.objects.filter(name__contains=keyword).order_by('name')
     return render(request, 'phone/result.html', {'contacts': contacts, 'keyword': keyword})
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+
+        phone = ContactInfo.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email
+        )
+        return redirect('phone:list')
+    return render(request, 'phone/create.html')
+
+def detail(request, id):
+    contact = get_object_or_404(ContactInfo, id=id)
+    return render(request, 'phone/detail.html', {'contact': contact})
+
+def update(request, id):
+    contact = get_object_or_404(ContactInfo, id=id)
+    if request.method == "POST":
+        contact.name = request.POST.get('name')
+        contact.phone_num = request.POST.get('phone_num')
+        contact.email = request.POST.get('email')
+        contact.save()
+        return redirect('phone:list')
+    return render(request,'phone/update.html', {'contact': contact})   
+
+def delete(request, id):
+    contact = get_object_or_404(ContactInfo, id=id)
+    if request.method == "POST":
+        contact.name = request.POST.get('name')
+        contact.delete()
+        return redirect('phone:list')
+    return render(request,'phone/delete.html', {'contact': contact})  
+
+
