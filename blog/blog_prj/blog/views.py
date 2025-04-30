2a1a47e78712a687d29914434b01ba658319e94e
unknown <seahorse2001@naver.com>
2025-04-30 13:46:48 +0900

    [0430] 이유정_blog_5차세션

--- blog/blog_prj/blog/views.py
+++ blog/blog_prj/blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 
 def list(request):
     posts = Post.objects.all().order_by('-id')
     return render(request, 'blog/list.html', {'posts':posts})
 
+@login_required
 def create(request):
     if request.method == "POST":
         title = request.POST.get('title')
-        concent = request.POST.get('content')
+        content = request.POST.get('content')
 
         post = Post.objects.create(
             title = title,
--- blog/blog_prj/blog_prj/settings.py
+++ blog/blog_prj/blog_prj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
